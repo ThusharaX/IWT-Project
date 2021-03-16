5aac554d21df22d8182a47448834527dc56598c3
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-03-17 04:56:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DB</title>
+    <title>Assignment 01</title>
 </head>
 <body>
     <?php
-	echo "Connected successfully";
+	echo "Hello LuciferX";
     ?>
 </body>
 </html>
