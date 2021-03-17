4ec9bcff41d3014d37d62a58e5a3482a2c92289b
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-03-18 01:43:59 +0530

    added about

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
       </button>
       <div class="navbar-menu detached">
         <ul class="navbar-links">
-          <li class="navbar-item"><a class="navbar-link" href="#">About</a></li>
+          <li class="navbar-item"><a class="navbar-link" href="/IWT-Assignment-01/about.php">About</a></li>
           <li class="navbar-item"><a class="navbar-link" href="#">Contact</a></li>
           <li class="navbar-item register"><a class="navbar-link" href="#">Register</a></li>
           <li class="navbar-item login"><a class="navbar-link" href="#">Login</a></li>
@@ -31,6 +31,7 @@
       </div>
     </nav>
 </header>
+
 
 
 <script src="navbar.js"></script>
