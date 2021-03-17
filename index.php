ec2a45834c96bcdc0fdddc974f7589b0ba11bb5f
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-03-17 20:57:19 +0530

    Navbar Added

--- index.php
+++ index.php
@@ -3,11 +3,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Assignment 01</title>
+    <link rel="stylesheet" href="navbar.css">
+    <title>මගුල</title>
 </head>
 <body>
+<header id="navbar">
+    <nav class="navbar-container container">
+      <a href="#" class="home-link">
+        <div class="navbar-logo"></div>
+        මගුල
+      </a>
+      <button type="button" class="navbar-toggle" aria-label="Open navigation menu">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+      </button>
+      <div class="navbar-menu detached">
+        <ul class="navbar-links">
+          <li class="navbar-item"><a class="navbar-link" href="#">About</a></li>
+          <li class="navbar-item"><a class="navbar-link" href="#">Contact</a></li>
+          <li class="navbar-item register"><a class="navbar-link" href="#">Register</a></li>
+          <li class="navbar-item login"><a class="navbar-link" href="#">Login</a></li>
+        </ul>
+        <ul>
+          
+        </ul>
+      </div>
+    </nav>
+</header>
     <?php
 	echo "Hello LuciferX";
     ?>
+
+<script src="navbar.js"></script>
 </body>
 </html>
