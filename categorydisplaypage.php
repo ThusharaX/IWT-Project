bd8a2b9399629cf24647820d6e4d71086e79d8ab
Gaween kanishka <gaweenkanishka@gmail.com>
2021-05-11 09:09:44 +0530

    Update categorydisplaypage.php
    upload whole code upto now

--- categorydisplaypage.php
+++ categorydisplaypage.php
@@ -6,10 +6,35 @@
 <link rel="stylesheet" href="./assets/css/categorydisplay.css">
 
 <!-- Gaween -->
-<!-- Type your code here -->
-<section>
-    <h1 class="main-title">Category Display Page</h1>
-</section>
+<!-- Type your code here -->	<center>
+		
+			
+<video width="1200" height="600" controls="controls" autoplay="autoplay" loop="loop" >
+  <source src="cover.mp4" type="video/mp4">
+</video>
+
+<table width="1230" height="500" border="0">
+	
+	
+  <tbody>
+    <tr>
+      <td><img src="Cyber Monday Animated Social Media Square Post Template _1_.gif" width="200" height="200" ></td>
+      <td><img src="Cyber Monday Animated Social Media Square Post Template _2_.gif" width="200" height="200" ></td>
+      <td><img src="Cyber Monday Animated Social Media Square Post Template _3_.gif" width="200" height="200" ></td>
+      <td><img src="Cyber Monday Animated Social Media Square Post Template _4_.gif" width="200" height="200" ></td>
+      <td><img src="Cyber Monday Animated Social Media Square Post Template _5_.gif" width="200" height="200" ></td>
+    </tr>
+    <tr>
+      <td><img src="Cyber Monday Animated Social Media Square Post Template _6_.gif" width="200" height="200" ></td>
+      <td><img src="Cyber Monday Animated Social Media Square Post Template _7_.gif" width="200" height="200" ></td>
+      <td><img src="Cyber Monday Animated Social Media Square Post Template _8_.gif" width="200" height="200" ></td>
+      <td><img src="Cyber Monday Animated Social Media Square Post Template _9_.gif" width="200" height="200" ></td>
+      <td><img src="Cyber Monday Animated Social Media Square Post Template.gif" width="200" height="200" ></td>
+    </tr>
+  </tbody>
+</table>
+
+	  <img src="category background.jpg" width="1230" height="750" >
 
 
 <script src="./assets/js/categorydisplay.js"></script>
