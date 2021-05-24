a9b5a5fd5d21be1b128ad5944485256982bcb7bc
Chamath Jayasekara <79747187+ChamathJayasekara99@users.noreply.github.com>
2021-05-24 15:02:20 +0530

    adsEdit fixed

--- adsEdit.php
+++ adsEdit.php
@@ -1,100 +1,10 @@
 <?php
- $title = 'Vendor Dashboard'; include("header.php");
- 	
-	     if(isset($_POST['save'])){
-			 // $target_file
-			  //assigning the name of the file to a variable
-  $fileName=$_FILES["images"]["name"];
- //assigning the size of the file to a variable
- $fileSize=$_FILES["images"]["size"];
- 
- 
- //Assigning temporary name to a variable
- $fileTempName=$_FILES["images"]["tmp_name"];  
- 
- //create directory
- //changed
-  $target_dir="./Uploads/adsImage/";
-
-  //contains the path
-  $target_file=$target_dir.basename($fileName);
-  
-  $uploadOk =true;
-  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-  
-  
-  //Check if the file is actual image or not
-  $check = getimagesize($fileTempName);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk =true;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = false;
-  }
-
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk =false;
-}
-
-// Check file size
-if ($fileSize > 2000000) {
-  echo "<script>alert('Too large')</script>";
-  $uploadOk =false;
-}
-
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-  $uploadOk =false;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk ==false) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} 
-else 
-  if (move_uploaded_file($fileTempName, $target_file)) {
-	  echo "<br>";
-    echo "The file ". htmlspecialchars( basename( $fileName)). " has been uploaded.";
-  } else {
-    echo "Sorry, there was an error uploading your file.";
-  }
- 			  
-			   $title=$_POST["title"];
-			   $adsDescription=$_POST["adsDescription"];
-			   $mobile=$_POST["mobile"];
-			  
-	//$sqlstmt="INSERT INTO Advertisement_payment(paymentID,catID,title,addDescription,mobile,addImageLoc,publishDateTime,status,amount,pay_type,pymntDescription,vendorID) 
-            		 
-			 $sqlstmt="UPDATE Advertisement_payment
- 			           SET title='$title',addDescription='$adsDescription',mobile=$mobile,addImageLoc='$target_file'
-					   WHERE adID=$adID;";
-			 if(mysqli_query($conn,$sqlstmt)){
-				 header("Location:adsInventory.php?EditedSuccessfully");
-			 }else{
-				 echo "Error : ".$conn->error;
-			 }			 
-		 }
-		
- ?>
- <?php
+  $title = 'Vendor Dashboard'; include("header.php");
   $adID=$_GET['id'];
   $adtitle=$_GET['title'];
   $adsDescription=$_GET['addes'];
   $mobile=$_GET['mobile'];
   $imglocation=$_GET['imglocation'];
-//BY IT20654962
-    // Dynamic Header
-   
-   
- //By IT20654962
-
-  //id=$row[adID]&title=$row[title]&addes=$row[addDescription]&mobile=$row[mobile]&imglocation=$row[addImageLoc]
-
 ?>
 
 <div class="alternative">
@@ -124,31 +34,35 @@ else
 					
 <div class="ads">
        <div class='display'>
-		 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST">
+		 <form action="adsEditandSave.php" method="POST" enctype="multipart/form-data">
              <table border='1px solid #ddd'>
-		      <tr>
-			     <td><img src="<?php echo $imglocation;?>" height='200' width='100%'/><br/>
-			     <input id="image" type="file" name="images"/></td>
 			  
-			  </tr>
-		 
-          <tr>
-		      <td>
-		         <input id="title" type="text" name="title" value="<?php echo $adtitle;?>"/>
-		  
-		     </td>
-		  </tr>			
-          <tr>
-		      <td>
-		         <textarea id="adsDescription" cols="5" rows="7" name="adsDescription "><?php echo htmlspecialchars($adsDescription);?></textarea>
-		      </td>
-		  </tr>
+		      <tr>
+			     <input type="hidden" name="adID" value="<?php echo $adID;?>">  
+			     <td id="alignment" rowspan=6><img src="<?php echo $imglocation;?>" height='200' width='100%'/><br/>
+			     <input id="imageUpFile" type="file" name="images"/></td>
+			  
+			  
+		      </tr>
+          
+		      
+			  <tr>
+		       <td>  <input id="title" type="text" name="title" value="<?php echo $adtitle;?>"/></td>
+		      </tr> 
+		     
+		  		
+          
+		  <tr>    
+		     <td><textarea id="adsDescription" cols="5" rows="7" name="adsDescription"><?php echo htmlspecialchars($adsDescription);?></textarea> </td>
+		     
+		</tr>  
         
-          <tr>
-		   <td>
-		       <input id="mobile" type="text" name="mobile" value="<?php echo $mobile;?>"/>
-		   </td>
-		  </tr>
+          
+	<tr>	   
+		       <td> <input id="mobile" type="text" name="mobile" value="<?php echo $mobile;?>"/> </td>
+</tr>	
+
+		  
           <tr>
 		     <td>
 			    <input id="submit" name="save" type="submit" value="save" />
@@ -166,10 +80,6 @@ else
 		  </table>
 		  </form>
 		 </div>
-    <?php 
-	  	
-		
-	?>
 
 
     </div>
---
+++ adsEditandSave.php
@@ -0,0 +1,88 @@
+<?php
+include './src/dbh.php';
+include 'ImageCheckAdvertisements.php';
+if(isset($_POST['save'])){
+	$adID=$_POST["adID"];
+	
+	// $target_file
+	//$target_file=imageChecking("images");
+			 
+			  //assigning the name of the file to a variable
+ $fileName=$_FILES["images"]["name"];
+ //assigning the size of the file to a variable
+ $fileSize=$_FILES["images"]["size"];
+ 
+ 
+ //Assigning temporary name to a variable
+ $fileTempName=$_FILES["images"]["tmp_name"];  
+ 
+ //create directory
+ //changed
+  $target_dir="./Uploads/adsImage/";
+
+  //contains the path
+  $target_file=$target_dir.basename($fileName);
+  
+  $uploadOk =true;
+  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+  
+  
+  //Check if the file is actual image or not
+  $check = getimagesize($fileTempName);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk =true;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = false;
+  }
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk =false;
+}
+
+// Check file size
+if ($fileSize > 2000000) {
+  echo "<script>alert('Too large')</script>";
+  $uploadOk =false;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk =false;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk ==false) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} 
+else 
+  if (move_uploaded_file($fileTempName, $target_file)) {
+	  echo "<br>";
+    echo "The file ". htmlspecialchars( basename( $fileName)). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+ 			  
+			   $title=$_POST["title"];
+			   $adsDescription=$_POST["adsDescription"];
+			   $mobile=$_POST["mobile"];
+			  
+	//$sqlstmt="INSERT INTO Advertisement_payment(paymentID,catID,title,addDescription,mobile,addImageLoc,publishDateTime,status,amount,pay_type,pymntDescription,vendorID) 
+            		 
+			 $sqlstmt="UPDATE Advertisement_payment
+ 			           SET title='$title',addDescription='$adsDescription',mobile=$mobile,addImageLoc='$target_file'
+					   WHERE adID=$adID;";
+			 if(mysqli_query($conn,$sqlstmt)){
+				 echo "<script>alert('$target_file')</script>";
+				header("Location:adsInventory.php?id=&title=&addes=&mobile=&imglocation=");
+			 }else{
+				 echo "Error : ".$conn->error;
+			 }			 
+		 }
+		
+?>
