a1bcf01610bda948e1894f1c4e17a138b1c19aaf
Gaween kanishka <gaweenkanishka@gmail.com>
2021-05-24 21:36:38 +0530

    rearranged gallery
    arranged gallery images

--- gallery.php
+++ gallery.php
@@ -8,40 +8,44 @@
 <!-- Gaween -->
 <!-- Type your code here -->
 <section>
+
+
 <h1 class="main-title"><strong>Gallery Page</strong></h1>
-   
+  <center>
     <video width="1200" height="500" autoplay loop="loop" >
         <source src="./assets/video/heroVideo.mp4" type="video/mp4">
     </video>
+</center> 
+<img src="./assets/img/gallery/11.jpg" width="1250" height="auto">
 
-
- <center>
-<div>
-<img src="./assets/img/gallery/11.jpg" width="1360" height="auto">
+<main class="grid">
 <img src="./assets/img/gallery/10.jpg" width="600" height="auto">
-
 <img src="./assets/img/gallery/1.jpg" width="610" height="auto">
 <img src="./assets/img/gallery/2.jpg" width="600" height="auto">
 <img src="./assets/img/gallery/3.jpg" width="600" height="auto">
-<img src="./assets/img/gallery/13.jpg" width="1200" height="auto">
+
 <img src="./assets/img/gallery/4.jpg" width="600" height="auto">
 <img src="./assets/img/gallery/5.jpg" width="600" height="auto">
 <img src="./assets/img/gallery/6.jpg" width="600" height="auto">
 <img src="./assets/img/gallery/7.jpg" width="600" height="auto">
 <img src="./assets/img/gallery/8.jpg" width="600" height="auto">
 
-<img src="./assets/img/gallery/12.jpg" width="600" height="auto">
+<img src="./assets/img/gallery/20.jpg" width="1300" height="auto">
+<img src="./assets/img/gallery/13.jpg" width="1200" height="auto">
 <img src="./assets/img/gallery/18.jpg" width="1200" height="auto">
 <img src="./assets/img/gallery/14.jpg" width="600" height="auto">
+<img src="./assets/img/gallery/12.jpg" width="600" height="auto">
 <img src="./assets/img/gallery/15.jpg" width="600" height="auto">
+</main>
+<center>
 <img src="./assets/img/gallery/16.jpg" width="600" height="auto">
 <img src="./assets/img/gallery/17.jpg" width="600" height="auto">
-<img src="./assets/img/gallery/20.jpg" width="1300" height="auto">
-<img src="./assets/img/gallery/9.jpg" width="1200" height="auto">
+
+
 <img src="./assets/img/gallery/22.jpg" width="1200" height="auto">
 <img src="./assets/img/gallery/21.jpg" width="1200" height="auto">
-  </center> 
-   </div> 
+
+</center>  
 </section>
 
 
