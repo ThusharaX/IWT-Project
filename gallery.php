eb931a4f9fca59ec6fbd262188e8620bb396f931
Gaween kanishka <gaweenkanishka@gmail.com>
2021-05-31 19:18:35 +0530

    modyfy gallery
    gallery modify

--- gallery.php
+++ gallery.php
@@ -20,27 +20,20 @@
     <video width="1250" height="auto" autoplay loop="loop" >
         <source src="./assets/video/Video1.mp4" type="video/mp4">
     </video>
+	<img src="./assets/img/gallery/21.jpg" width="1200" height="auto">
+	<img src="./assets/img/gallery/new/b.jpeg" width="1200" height="auto">
+	<img src="./assets/img/gallery/new/c.jpeg" width="1200" height="auto">	
+	
 
 
 
-
-<img src="./assets/img/gallery/21.jpg" width="1200" height="auto">
-<img src="./assets/img/gallery/new/b.jpeg" width="1200" height="auto">
-
-
-
-<img src="./assets/img/gallery/new/d.jpeg" width="600" height="auto">
-<img src="./assets/img/gallery/new/e.jpeg" width="600" height="auto">
-<img src="./assets/img/gallery/new/f.jpeg" width="600" height="auto">
-<img src="./assets/img/gallery/new/g.jpeg" width="600" height="auto">
-<img src="./assets/img/gallery/new/h.jpeg" width="600" height="auto">
-<img src="./assets/img/gallery/new/i.jpeg" width="600" height="auto">
-
-
-
-
-
-<main class="grid">	
+<main class="gallery1">	
+	<img src="./assets/img/gallery/new/d.jpeg" width="600" height="auto">
+	<img src="./assets/img/gallery/new/e.jpeg" width="600" height="auto">
+	<img src="./assets/img/gallery/new/f.jpeg" width="600" height="auto">
+	<img src="./assets/img/gallery/new/g.jpeg" width="600" height="auto">
+	<img src="./assets/img/gallery/new/h.jpeg" width="600" height="auto">
+	<img src="./assets/img/gallery/new/i.jpeg" width="600" height="auto">
 
 	<img src="./assets/img/gallery/new/j.jpeg" width="600" height="auto">
 		<img src="./assets/img/gallery/new/k.jpeg" width="600" height="auto">
@@ -50,19 +43,25 @@
 						<img src="./assets/img/gallery/new/o.jpeg" width="600" height="auto">
 
 
+</main>
 
 
+
+<br><br>
+
+<hr>
+
+
+<main class="grid">	
 <img src="./assets/img/gallery/16.jpg" width="600" height="auto">
 <img src="./assets/img/gallery/1.jpg" width="610" height="auto">
 <img src="./assets/img/gallery/17.jpg" width="600" height="auto">
 <img src="./assets/img/gallery/3.jpg" width="600" height="auto">
-
 <img src="./assets/img/gallery/4.jpg" width="600" height="auto">
 <img src="./assets/img/gallery/5.jpg" width="600" height="auto">
 <img src="./assets/img/gallery/6.jpg" width="600" height="auto">
 <img src="./assets/img/gallery/7.jpg" width="600" height="auto">
 <img src="./assets/img/gallery/8.jpg" width="600" height="auto">
-
 <img src="./assets/img/gallery/20.jpg" width="1300" height="auto">
 <img src="./assets/img/gallery/13.jpg" width="1200" height="auto">
 <img src="./assets/img/gallery/18.jpg" width="1200" height="auto">
@@ -71,7 +70,11 @@
 <img src="./assets/img/gallery/15.jpg" width="600" height="auto">
 </main>
 	
-<main class="grid1">
+
+
+<hr
+
+<main class="gallery2">	
 	<img src="./assets/img/gallery/new/112.jpeg" width="600" height="auto">
 	<img src="./assets/img/gallery/new/114.jpeg" width="600" height="auto">
 	<img src="./assets/img/gallery/new/115.jpeg" width="600" height="auto">
@@ -82,9 +85,9 @@
 	<img src="./assets/img/gallery/new/120.jpeg" width="600" height="auto">
 	<img src="./assets/img/gallery/new/121.jpeg" width="600" height="auto">
 	<img src="./assets/img/gallery/new/122.jpeg" width="600" height="auto">
-	<img src="./assets/img/gallery/new/100.jpeg" width="600" height="auto">
+	<img src="./assets/img/gallery/new/100.jpeg" width="1250" height="auto">
 	<img src="./assets/img/gallery/new/101.jpeg" width="600" height="auto">
-	<img src="./assets/img/gallery/new/102.jpeg" width="600" height="auto">
+
 	<img src="./assets/img/gallery/new/103.jpeg" width="600" height="auto">
 	<img src="./assets/img/gallery/new/104.jpeg" width="600" height="auto">
 	<img src="./assets/img/gallery/new/105.jpeg" width="600" height="auto">
@@ -93,9 +96,11 @@
 	<img src="./assets/img/gallery/new/108.jpeg" width="1300" height="auto">
 	<img src="./assets/img/gallery/new/109.jpeg" width="1300" height="auto">
 	<img src="./assets/img/gallery/new/110.jpeg" width="600" height="auto">
-	<img src="./assets/img/gallery/new/111.jpeg" width="600" height="auto">
+</main>
 
-	</main>
+
+	<img src="./assets/img/gallery/new/111.jpeg" width="1250" height="auto">
+
 
 <img src="./assets/img/gallery/11.jpg" width="1250" height="auto">
 
