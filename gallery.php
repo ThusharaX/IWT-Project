ff3b08251ce6a8ec947ab6a53563ab8af0c807d4
Chamath Jayasekara <79747187+ChamathJayasekara99@users.noreply.github.com>
2021-05-31 16:29:05 +0530

    Last commit

--- gallery.php
+++ gallery.php
@@ -20,34 +20,34 @@
 
 <br>
 
-<img src="./assets/img/gallery/11.jpg" width="1250" height="auto">
+<img src="./assets/img/gallery/11.jpg" id="5" width="1250" height="auto">
 <center>
 <main class="grid">
 	
 <img src="./assets/img/gallery/10.jpg" width="600" height="auto">
 <img src="./assets/img/gallery/1.jpg" width="610" height="auto">
 <img src="./assets/img/gallery/2.jpg" width="600" height="auto">
-<img src="./assets/img/gallery/3.jpg" width="600" height="auto">
+<img src="./assets/img/gallery/3.jpg" id="6" width="600" height="auto">
 
 <img src="./assets/img/gallery/4.jpg" width="600" height="auto">
-<img src="./assets/img/gallery/5.jpg" width="600" height="auto">
-<img src="./assets/img/gallery/6.jpg" width="600" height="auto">
-<img src="./assets/img/gallery/7.jpg" width="600" height="auto">
-<img src="./assets/img/gallery/8.jpg" width="600" height="auto">
+<img src="./assets/img/gallery/5.jpg" id="4" width="600" height="auto">
+<img src="./assets/img/gallery/6.jpg" id="2" width="600" height="auto">
+<img src="./assets/img/gallery/7.jpg" id="3" width="600" height="auto">
+<img src="./assets/img/gallery/8.jpg" id="7" width="600" height="auto">
 
-<img src="./assets/img/gallery/20.jpg" width="1300" height="auto">
+<img src="./assets/img/gallery/20.jpg" id="12" width="1300" height="auto">
 <img src="./assets/img/gallery/13.jpg" width="1200" height="auto">
-<img src="./assets/img/gallery/18.jpg" width="1200" height="auto">
+<img src="./assets/img/gallery/18.jpg" id="11" width="1200" height="auto">
 <img src="./assets/img/gallery/14.jpg" width="600" height="auto">
 <img src="./assets/img/gallery/12.jpg" width="600" height="auto">
-<img src="./assets/img/gallery/15.jpg" width="600" height="auto">
+<img src="./assets/img/gallery/15.jpg" id="1" width="600" height="auto">
 
 	
 	<img src="./assets/img/gallery/new/112.jpeg" width="600" height="auto">
 	<img src="./assets/img/gallery/new/113.jpeg" width="600" height="auto">
-	<img src="./assets/img/gallery/new/114.jpeg" width="600" height="auto">
-	<img src="./assets/img/gallery/new/115.jpeg" width="600" height="auto">
-	<img src="./assets/img/gallery/new/116.jpeg" width="600" height="auto">
+	<img src="./assets/img/gallery/new/114.jpeg" id="7" width="600" height="auto">
+	<img src="./assets/img/gallery/new/115.jpeg" id="5" width="600" height="auto">
+	<img src="./assets/img/gallery/new/116.jpeg" id="11" width="600" height="auto">
 	<img src="./assets/img/gallery/new/117.jpeg" width="600" height="auto">
 	<img src="./assets/img/gallery/new/118.jpeg" width="600" height="auto">
 	<img src="./assets/img/gallery/new/119.jpeg" width="600" height="auto">
@@ -55,7 +55,7 @@
 	<img src="./assets/img/gallery/new/121.jpeg" width="600" height="auto">
 	<img src="./assets/img/gallery/new/122.jpeg" width="600" height="auto">
 	<img src="./assets/img/gallery/new/100.jpeg" width="600" height="auto">
-	<img src="./assets/img/gallery/new/101.jpeg" width="600" height="auto">
+	<img src="./assets/img/gallery/new/101.jpeg" id="7" width="600" height="auto">
 	<img src="./assets/img/gallery/new/102.jpeg" width="600" height="auto">
 	<img src="./assets/img/gallery/new/103.jpeg" width="600" height="auto">
 	<img src="./assets/img/gallery/new/104.jpeg" width="600" height="auto">
@@ -63,18 +63,18 @@
 	<img src="./assets/img/gallery/new/106.jpeg" width="600" height="auto">
 	<img src="./assets/img/gallery/new/107.jpeg" width="600" height="auto">
 	<img src="./assets/img/gallery/new/108.jpeg" width="600" height="auto">
-	<img src="./assets/img/gallery/new/109.jpeg" width="600" height="auto">
+	<img src="./assets/img/gallery/new/109.jpeg" id="12" width="600" height="auto">
 	<img src="./assets/img/gallery/new/110.jpeg" width="600" height="auto">
-	<img src="./assets/img/gallery/new/111.jpeg" width="600" height="auto">
+	<img src="./assets/img/gallery/new/111.jpeg" id="4" width="600" height="auto">
 	</center>
 	</main>
 
 
-<img src="./assets/img/gallery/16.jpg" width="600" height="auto">
-<img src="./assets/img/gallery/17.jpg" width="600" height="auto">
+<img src="./assets/img/gallery/16.jpg" id="9" width="600" height="auto">
+<img src="./assets/img/gallery/17.jpg" id="10" width="600" height="auto">
 
 
-<img src="./assets/img/gallery/22.jpg" width="1200" height="auto">
+<img src="./assets/img/gallery/22.jpg" id="8" width="1200" height="auto">
 <hr>
 <hr>
 <hr>
